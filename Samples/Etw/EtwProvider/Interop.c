d5c9193347a0f73e81cc4ced3cef23ef5835ea82
pekabon <skeevee@gmail.com>
2013-09-19 17:28:07 +0400

    publishing events

--- Samples/Etw/EtwProvider/Interop.c
+++ Samples/Etw/EtwProvider/Interop.c
@@ -1,6 +1,6 @@
 typedef int LPCGUID;
 typedef int HANDLE;
-typedef int GUID;
+
 typedef unsigned short USHORT;
 typedef unsigned char UCHAR;
 typedef unsigned long ULONG;
@@ -8,23 +8,63 @@ typedef unsigned long long LARGE_INTEGER;
 typedef unsigned long long ULONGLONG;
 typedef unsigned long long ULONG64;
 
+typedef ULONG64 TRACEHANDLE;
+typedef void*	POBJECT;
+typedef struct _GUID {
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+typedef struct _MOF_FIELD {
+
+  ULONG64 DataPtr;
+  ULONG   Length;
+  ULONG   DataType;
+
+} MOF_FIELD, *PMOF_FIELD;
+
 typedef struct  _TRACE_GUID_REGISTRATION {
     LPCGUID Guid;            // Guid of data block being registered or updated.
     HANDLE RegHandle;        // Guid Registration Handle is returned.
 } TRACE_GUID_REGISTRATION, *PTRACE_GUID_REGISTRATION;
 
-typedef struct _EVENT_TRACE_HEADER  {
-    USHORT      Size;
-    USHORT      FieldTypeFlags;
-    UCHAR		Type;
-    UCHAR		Level;               
-    USHORT		Version;             
-    ULONG       ThreadId;    
-    ULONG       ProcessId;   
-    LARGE_INTEGER   TimeStamp;   
-    GUID        Guid;            
-    ULONG		KernelTime;          
-    ULONG		UserTime;            
+typedef struct _EVENT_TRACE_HEADER {
+  USHORT        Size;
+  union {
+    USHORT FieldTypeFlags;
+    struct {
+      UCHAR HeaderType;
+      UCHAR MarkerFlags;
+    };
+  };
+  union {
+    ULONG  Version;
+    struct {
+      UCHAR  Type;
+      UCHAR  Level;
+      USHORT Version;
+    } Class;
+  };
+  ULONG         ThreadId;
+  ULONG         ProcessId;
+  LARGE_INTEGER TimeStamp;
+  union {
+    GUID      Guid;
+    ULONGLONG GuidPtr;
+  };
+  union {
+    struct {
+      ULONG ClientContext;
+      ULONG Flags;
+    };
+    struct {
+      ULONG KernelTime;
+      ULONG UserTime;
+    };
+    ULONG64 ProcessorTime;
+  };
 } EVENT_TRACE_HEADER, *PEVENT_TRACE_HEADER;
 
 typedef struct _EVENT_INSTANCE_HEADER {
@@ -63,3 +103,22 @@ typedef struct _EVENT_INSTANCE_HEADER {
   };
   ULONGLONG     ParentRegHandle;
 } EVENT_INSTANCE_HEADER;
+
+typedef struct _WNODE_HEADER {
+  ULONG BufferSize;
+  ULONG ProviderId;
+  union {
+    ULONG64 HistoricalContext;
+    struct {
+      ULONG Version;
+      ULONG Linkage;
+    };
+  };
+  union {
+    HANDLE        KernelHandle;
+    LARGE_INTEGER TimeStamp;
+  };
+  GUID  Guid;
+  ULONG ClientContext;
+  ULONG Flags;
+} WNODE_HEADER, *PWNODE_HEADER;
